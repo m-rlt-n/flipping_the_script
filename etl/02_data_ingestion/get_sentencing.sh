f263400045a4b28e0f9e295bf3d48ce0e6a40967
m-rlt-n <86677029+m-rlt-n@users.noreply.github.com>
2023-12-02 12:09:27 -0600

    pull data from API into local and transfer to HDFS

--- etl/02_data_ingestion/get_sentencing.sh
+++ etl/02_data_ingestion/get_sentencing.sh
@@ -10,5 +10,8 @@ headers=(-H "Accept: application/json" -H "X-App-Token: $apptoken")
 json_data=$(curl -s -X GET "$url" "${headers[@]}" | jq -r '.[] | flatten | @csv')
 
 # Create a CSV file and write the header and data
-echo "case_id,case_participant_id,received_date,offense_category,primary_charge,charge_id,charge_version_id,disposition_charged_offense_title,charge_count,disposition_date,disposition_charged_chapter,disposition_charged_class,disposition_charged_aoic,charge_disposition,sentence_judge,court_name,court_facility,sentence_phase,sentence_date,sentence_type,current_sentence,commitment_type,length_of_case_in_days,age_at_incident,race,gender,incident_begin_date,law_enforcement_agency,arrest_date,felony_review_date,felony_review_result,arraignment_date,updated_offense_category" > mnicolas_data/sentencing.csv
-echo "$json_data" >> mnicolas_data/sentencing.csv
+echo "case_id,case_participant_id,received_date,offense_category,primary_charge,charge_id,charge_version_id,disposition_charged_offense_title,charge_count,disposition_date,disposition_charged_chapter,disposition_charged_class,disposition_charged_aoic,charge_disposition,sentence_judge,court_name,court_facility,sentence_phase,sentence_date,sentence_type,current_sentence,commitment_type,length_of_case_in_days,age_at_incident,race,gender,incident_begin_date,law_enforcement_agency,arrest_date,felony_review_date,felony_review_result,arraignment_date,updated_offense_category" > data/sentencing.csv
+echo "$json_data" >> data/sentencing.csv
+
+# Write to data lake
+hadoop fs -copyFromLocal -f data/sentencing.csv mnicolas/sentencing.csv
