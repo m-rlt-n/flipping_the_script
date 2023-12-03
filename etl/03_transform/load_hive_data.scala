9b82da6700c1a3e4c25596e7ae4ca50f6b537bdc
m-rlt-n <86677029+m-rlt-n@users.noreply.github.com>
2023-12-03 15:44:38 -0600

    join on more criteria

--- etl/03_transform/load_hive_data.scala
+++ etl/03_transform/load_hive_data.scala
@@ -1,9 +1,10 @@
-import org.apache.spark.sql.SparkSession
+// Dependencies
+import org.apache.spark.sql.{SparkSession, SaveMode}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 // Spark Session
-val spark = SparkSession.builder.appName("CountyDataTransform").getOrCreate()
+val spark = SparkSession.builder.appName("LoadHiveData").getOrCreate()
 
 // Set the configuration
 spark.conf.set("spark.sql.legacy.json.allowEmptyString.enabled", "true")
@@ -45,9 +46,9 @@ val init_schema = StructType(
 
 // Load data into initDF
 val initDF = spark.read.schema(init_schema).option("multiline",true).json("mnicolas/initiation_*.json")
-initDF.show()
+// initDF.show()
 val initSize = initDF.count()
-println(s"data size: $initSize")
+println(s"initDF size: $initSize")
 
 // Schema for Disposition data
 val dispo_schema = StructType(
@@ -85,9 +86,9 @@ val dispo_schema = StructType(
 
 // Load disposition data into dispoDF
 val dispoDF = spark.read.schema(dispo_schema).option("multiline",true).json("mnicolas/disposition_*.json")
-dispoDF.show()
+// dispoDF.show()
 val dispoSize = dispoDF.count()
-println(s"data size: $dispoSize")
+println(s"dispoDF size: $dispoSize")
 
 // Schema for Sentencing data
 val sent_schema = StructType(
@@ -133,21 +134,35 @@ val sent_schema = StructType(
 
 // Load sentencing data into sentDF
 val sentDF = spark.read.schema(sent_schema).option("multiline",true).json("mnicolas/sentencing_*.json")
-sentDF.show()
+// sentDF.show()
 val sentSize = sentDF.count()
-println(s"data size: $sentSize")
+println(s"sentDF size: $sentSize")
 
 // Find intersection of ids across the data sets
-val commonIDs = dispoDF.select("case_id").intersect(sentDF.select("case_id"))
+val joinColumns: Seq[String] = Seq("case_id", "case_participant_id", "charge_version_id")
+val commonIDs = initDF.selectExpr(joinColumns: _*)
+  .intersect(dispoDF.selectExpr(joinColumns: _*))
+  .intersect(sentDF.selectExpr(joinColumns: _*))
 val commonSize = commonIDs.count()
-println(s"data size: $commonSize")
+println(s"commonIDs size: $commonSize")
 
 // Specify columns to bring from each DataFrame
-val initDFSubset = initDF.select("case_id", "updated_offense_category", "bond_type_current", "bond_amount_current", "bond_electronic_flag_current")
-val dispoDFSubset = dispoDF.select("case_id", "chapter", "act", "section", "class", "aoic", "event", "event_date", "law_enforcement_agency", "judge", "unit", "incident_end_date", "received_date", "charge_count")
-val sentDFSubset = sentDF.select("case_id", "case_participant_id", "offense_category", "primary_charge", "charge_id", "charge_version_id", "disposition_charged_offense_title", "age_at_incident", "gender", "race", "incident_begin_date", "arrest_date", "arraignment_date", "updated_offense_category", "sentence_judge", "court_name", "court_facility", "sentence_phase", "sentence_date", "sentence_type", "current_sentence", "commitment_type", "commitment_term", "commitment_unit", "length_of_case_in_days", "felony_review_date", "felony_review_result")
+val initDFSubset = initDF.select("case_id", "case_participant_id", "charge_version_id", "updated_offense_category", "bond_type_current", "bond_amount_current", "bond_electronic_flag_current")
+val dispoDFSubset = dispoDF.select("case_id", "case_participant_id", "charge_version_id", "chapter", "act", "section", "class", "aoic", "event", "event_date", "law_enforcement_agency", "judge", "unit", "incident_end_date", "received_date", "charge_count")
+val sentDFSubset = sentDF.select("case_id", "case_participant_id", "charge_version_id", "charge_id", "offense_category", "primary_charge", "disposition_charged_offense_title", "age_at_incident", "gender", "race", "incident_begin_date", "arrest_date", "arraignment_date", "sentence_judge", "court_name", "court_facility", "sentence_phase", "sentence_date", "sentence_type", "current_sentence", "commitment_type", "commitment_term", "commitment_unit", "length_of_case_in_days", "felony_review_date", "felony_review_result")
 
-//
-val joinedDF = commonIDs.join(initDFSubset, Seq("case_id"), "inner").join(dispoDFSubset, Seq("case_id"), "inner").join(sentDFSubset, Seq("case_id"), "inner")
+// Create Joined DF and save to Hive table
+val intermediateJoin1 = commonIDs.join(initDFSubset, joinColumns, "inner")
+val intermediateJoin2 = intermediateJoin1.join(dispoDFSubset, joinColumns, "inner")
+val joinedDF = intermediateJoin2.join(sentDFSubset, joinColumns, "inner")
 
+//val joinedDF = commonIDs.join(initDFSubset, joinColumns, "inner").join(dispoDFSubset, joinColumns, "inner").join(sentDFSubset, joinColumns, "inner")
 joinedDF.show()
+val joinedSize = joinedDF.count()
+println(s"joinedDF size: $joinedSize")
+
+// Write DataFrame to Hive table
+joinedDF.write
+  .mode(SaveMode.Overwrite)  // Choose the SaveMode: Overwrite, Append, ErrorIfExists, Ignore
+  .saveAsTable("cook_county_data")
+// Done
